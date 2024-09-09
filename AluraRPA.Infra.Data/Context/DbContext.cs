2013fc469828c85c7f3253e36b6d4e583ec33aef
danilo.py <danilo.py@outlook.com>
2024-09-09 11:38:03 -0300

    Adicionar arquivos de projeto.

---
+++ AluraRPA.Infra.Data/Context/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Options;
+
+namespace AluraRPA.Infra.Data.Context
+{
+    public class DbContext: IDbContext
+    {
+        private readonly IDbConnection _connection;
+
+        public DbContext(IOptions<ConnectionStringOptions> databaseConfig)
+        {
+            //connection = new SqlConnection(databaseConfig.Value.ConnectionString);
+            _connection.Open();
+        }
+
+        public IDbConnection Connection => _connection;
+
+        public void Dispose()
+        {
+            if (_connection.State != ConnectionState.Closed)
+                _connection.Close();
+        }
+
+    }
+}
---
+++ AluraRPA.Infra.Data/Context/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraRPA.Infra.Data.Context
+{
+    public interface IDbContext : IDisposable
+    {
+        IDbConnection Connection { get; }
+    }
+}
